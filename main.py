28876a759b14f73044b4d04b7b479924faee036a
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-23 18:15:18 +0700

    Fix some math problems with moving point to next line

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ colors = colorgram.extract("hirst_art/HirstArt.jpg", 15)
 t.setworldcoordinates(0, 0, sc.window_width(), sc.window_height())
 tim.pensize(30)
 
-
+#move forward and draw one circle
 def draw_circle():
     rand_choice = random.choice(colors)
     tup = (rand_choice.rgb[0], rand_choice.rgb[1], rand_choice.rgb[2])
@@ -17,17 +17,23 @@ def draw_circle():
     tim.forward(50)
     tim.pencolor(tup)
     tim.pendown()
+    #only call pendown doesn't draw a circle. It needs to move point.
     tim.forward(2)
     tim.penup()
 
-
+#moves point 50 from first circle from the previous line
 def next_line(i):
-    tim.setpos(0, 100*i)
+    tim.setpos(0, 80*i+50)
 
 tim.speed("fastest")
 tim.penup()
-tim.setpos(0, 15)
-for i in range(1, int(sc.window_height()/96)+1):
+tim.setpos(0, 50)
+
+# Distance between circles vertically: 50
+# Circle size: 30
+# There must be a distance of at least 50 between the upper and lower boundaries.
+# Subtract 50 to immediately fix the distances for the upper and lower borders.
+for i in range(1, round((sc.window_height()-50)/80)+1):
     for j in range(1, int(sc.window_width()/50)-1):
         draw_circle()
     next_line(i)
